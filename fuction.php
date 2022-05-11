9ae9cb5a9dbd6a6afa3fbad6fbf3fe57a38cc47f
brogode <godfreymanjo@gmail.com>
2022-05-11 19:24:01 +0200

    repeatcontent

--- fuction.php
+++ fuction.php
@@ -1,5 +1,14 @@
 <?php
-function hello($text) : void {
+function hello(string $text): void {
     echo $text;
 }
 hello('parameter');
+
+function repeatcontent(int $count = 1): void{
+    if($count == 0){
+        return ;
+    }
+    hello('parameter'.$count);
+    repeatcontent(--$count);
+}
+repeatcontent(5);
