d919bb48a7252e638bf33c140c687e9645f4534a
brogode <godfreymanjo@gmail.com>
2022-05-13 19:37:39 +0200

    Exercise7

--- calculator.php
+++ calculator.php
@@ -15,3 +15,21 @@ echo sum(10,2)."<br/>";
 echo diff(10,2)."<br/>";
 echo div(10,2)."<br/>";
 echo mult(10,2)."<br/>";
+
+function calculator(float $num1,string $opp,float $num2,): float{
+    if (!($opp == '+' or $opp == '-' or $opp == '/' or $opp == '*')){
+        return 0;
+    }
+    switch ($opp){
+        case '+': return sum($num1,$num2);
+        case '-': return diff($num1,$num2);
+        case '/': return div($num1,$num2);
+        case '*': return mult($num1,$num2);
+            
+    }
+}
+echo calculator(1,'+',2)."<br/>";
+echo calculator(1,'/',2)."<br/>";
+echo calculator(1,'*',2)."<br/>";
+echo calculator(1,'-',2)."<br/>";
+
